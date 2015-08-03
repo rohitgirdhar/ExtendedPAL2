093d73904c9240850a03e29994b96bf0c3d2a090
Rohit Girdhar <rgirdhar@andrew.cmu.edu>
2015-08-03 04:52:43 +0000

    update

---
+++ run_scripts/buildIndex_test.sh
@@ -0,0 +1,10 @@
+CODE_PATH=/home/rgirdhar/data/Work/Code/0002_Retrieval/Try/ScalableLSH/DiskE2LSH
+nice -n 10 $CODE_PATH/buildIndex.bin \
+    -d /srv2/rgirdhar/Work/Datasets/processed/0010_ExtendedPAL_moreTest/features/CNN/pool5_uni_normed_LMDB \
+    -n /srv2/rgirdhar/Work/Datasets/processed/0010_ExtendedPAL_moreTest/lists/NdxesTest.txt \
+    -c /srv2/rgirdhar/Work/Datasets/processed/0010_ExtendedPAL_moreTest/features/selsearch_boxes/counts.txt \
+    -s /srv2/rgirdhar/Work/Datasets/processed/0010_ExtendedPAL_moreTest/search/indexes/trainNneg.index \
+    -b 256 \
+    -t 1 \
+    -a 900 \
+    --nTrain 500000
