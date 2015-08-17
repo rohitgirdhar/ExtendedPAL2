eb60068e901e282f4df86198e2a1b16a382ec821
Rohit Girdhar <rgirdhar@andrew.cmu.edu>
2015-08-17 02:38:18 +0000

    train sims

--- run_scripts/computePairwiseSim.sh
+++ run_scripts/computePairwiseSim.sh
@@ -2,6 +2,6 @@ CODE_PATH=/home/rgirdhar/data/Work/Code/0002_Retrieval/Try/ScalableLSH/DiskE2LSH
 nice -n 10 $CODE_PATH/computePairwiseSim.bin \
     -d /srv2/rgirdhar/Work/Datasets/processed/0010_ExtendedPAL_moreTest/features/CNN/pool5_uni_normed_LMDB \
     -o /home/rgirdhar/data/Work/Datasets/processed/0010_ExtendedPAL_moreTest/features/aux/pairwise_matches \
-    -l /home/rgirdhar/data/Work/Datasets/processed/0010_ExtendedPAL_moreTest/lists/NdxesPeopleTest.txt \
+    -l /home/rgirdhar/data/Work/Datasets/processed/0010_ExtendedPAL_moreTest/lists/NdxesPeopleTrain.txt \
     -c /srv2/rgirdhar/Work/Datasets/processed/0010_ExtendedPAL_moreTest/features/selsearch_boxes/counts.txt
-#    -l /home/rgirdhar/data/Work/Datasets/processed/0006_ExtendedPAL/lists/NdxesPeopleTrain.txt \
+#    -l /home/rgirdhar/data/Work/Datasets/processed/0006_ExtendedPAL/lists/NdxesPeopleTest.txt \
